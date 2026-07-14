b8954acfeaa59f91e0dbcb55a4b22d6806a7890e
PRAVEEN <119511207+praveen-2528@users.noreply.github.com>
2026-07-14 14:37:21 +0530

    Added /login route to fix blank page redirect after password reset

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/reset-password" element={<ResetPassword />} />
       <Route path="/home" element={<Dashboard />} />
